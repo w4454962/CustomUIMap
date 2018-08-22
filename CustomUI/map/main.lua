f532a96ea007a139309d0d67511e9cecc5ad3810
w4454962 <1290875878@qq.com>
2018-08-22 17:57:03 +0800

    1

--- CustomUI/map/main.lua
+++ CustomUI/map/main.lua
@@ -8,54 +8,6 @@ require 'script'
 
 --[[
 
- local panel = {
-    id ='button_object',
-    type = "button",
-    x = 0,
-    y = 0,
-    w = 64,
-    h = 64,
-    toggle = true,
-    normal_image = "image/宝箱.png",
-    toggle_image = "image/blood1.png",
-}
-js.create(panel)
-
-
-local button = button_class.create("image/blood1.png",300,100,256,256)
-
-button.on_button_clicked = function (button)
-    print(button.id)
-end 
-
-
-
-message.hook = function (msg)
-
-    if msg.type == "key_down" and msg.code == 512 then 
-        print("aa")
-        
-    end 
-    return true
-end 
-]]
-
---[[
-local label2 =   {
-    id = 'name',
-    type = "label",
-    x = 500,
-    y = 500,
-    w = 64,
-    h = 64,
-    text = "名字",
-    font_size = 32,
-    normal_image = "image/blood1.png",
-}
-
-ui.InvokeFrontEndMethod("add_child","nil",json.encode(label2))
-
-
 local video = {
     id ='video_object',
     type = "video",
@@ -76,7 +28,7 @@ life_bar.create_bar(unit)
 
 
 japi.hide_interface(0)
-jass.TimerStart(jass.CreateTimer(),1,false,function ()
+jass.TimerStart(jass.CreateTimer(),0.1,false,function ()
     jass.DestroyTimer(jass.GetExpiredTimer())
     japi.set_black_borders(0,0)
 end)
